3404da054185937e6c0ca1aa3b74c0664b6707db
sudhanshusingh <ss4827@drexel.edu>
2021-05-23 04:41:00 +0530

    Final commit for Assignment 6 branch.

--- src/test/java/FullCommandValidatorTest.java
+++ src/test/java/FullCommandValidatorTest.java
@@ -15,7 +15,7 @@ public class FullCommandValidatorTest {
     }
 
     @Test
-    void create_valid_checking_command(){
+    void valid_create_checking_command(){
         String command = "Create Checking 12345678 1.2";
         assertTrue(fullCommandValidator.Validator(command).check_if_valid(command));
     }
@@ -40,7 +40,7 @@ public class FullCommandValidatorTest {
 
     @Test
     void create_invalid_savings_command(){
-        String command = "Create Saving 123456789 1.2";
+        String command = "Create Savings 123456789 1.2";
         assertFalse(fullCommandValidator.Validator(command).check_if_valid(command));
     }
 
@@ -51,7 +51,42 @@ public class FullCommandValidatorTest {
     }
 
     @Test
-    void create_valid_deposit_command(){
+    void duplicate_id_exists(){
+        Account account = new CheckingAccount(12345678, 1.2);
+        bank.addAccount(12345678, account);
+        String command = "Create Savings 12345678 1.2";
+        assertFalse(fullCommandValidator.Validator(command).check_if_valid(command));
+    }
+
+    @Test
+    void invalid_apr(){
+        String command = "Create Checking 12345678 [1.6]";
+        assertFalse(fullCommandValidator.Validator(command).check_if_valid(command));
+    }
+
+    @Test
+    void invalid_cd_amount(){
+        String command = "Create CD 12345678 1.2 100000";
+        assertFalse(fullCommandValidator.Validator(command).check_if_valid(command));
+    }
+
+    @Test
+    void negative_cd_amount(){
+        String command = "Create CD 1234578 1.2 -1000";
+        assertFalse(fullCommandValidator.Validator(command).check_if_valid(command));
+    }
+
+    @Test
+    void negative_apr(){
+        String command = "Create Checking -1.2";
+        assertFalse(fullCommandValidator.Validator(command).check_if_valid(command));
+    }
+
+
+    /////////////DEPOSIT TESTS////////////////
+
+    @Test
+    void valid_deposit_command(){
         account = new SavingsAccount(12345678 ,1.2);
         bank.addAccount(12345678, account);
         String command = "Deposit 12345678 400";
@@ -59,14 +94,43 @@ public class FullCommandValidatorTest {
     }
 
     @Test
-    void create_invalid_deposit_command(){
+    void invalid_deposit_command(){
         account = new Account("Savings", 12345678, 1.2, 0.0);
         bank.addAccount(12345678, account);
         String command = "Deposit Savings 34243424 890";
         assertFalse(fullCommandValidator.Validator(command).check_if_valid(command));
-
     }
 
+    @Test
+    void deposit_amount_too_high(){
+        account = new Account("Savings", 12345678, 1.2, 0.0);
+        bank.addAccount(12345678, account);
+        String command = "Deposit Savings 12345678 100000";
+        assertFalse(fullCommandValidator.Validator(command).check_if_valid(command));
+    }
 
+    @Test
+    void deposit_amount_negative(){
+        account = new Account("Savings", 12345678, 1.2, 0.0);
+        bank.addAccount(12345678, account);
+        String command = "Deposit Savings 12345678 -1000";
+        assertFalse(fullCommandValidator.Validator(command).check_if_valid(command));
+    }
+
+    @Test
+    void invalid_deposit_amount(){
+        account = new Account("Checking", 12345678, 1.2, 0.0);
+        bank.addAccount(12345678, account);
+        String command = "Deposit Checking 12345678 [1000]";
+        assertFalse(fullCommandValidator.Validator(command).check_if_valid(command));
+    }
+
+    @Test
+    void cant_deposit_into_CD_account(){
+        account = new Account("CD", 12345678, 1.2, 5000.0);
+        bank.addAccount(12345678, account);
+        String command = "Deposit CD 12345678 1000";
+        assertFalse(fullCommandValidator.Validator(command).check_if_valid(command));
+    }
 
 }
