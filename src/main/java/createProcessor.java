9a5748990790a687b137bfd685247508f23b99b7
sudhanshusingh <ss4827@drexel.edu>
2021-05-23 23:26:27 +0530

    Assignment 7 push - 2

---
+++ src/main/java/createProcessor.java
@@ -0,0 +1,15 @@
+public class createProcessor extends Processor{
+    public createProcessor(Bank bank){super(bank);}
+
+    public Processor generateCreate(String[] Array){
+        if(Array[1].equals("checking")){
+            return new checkingProcessor(bank);
+        }
+        else if(Array[1].equals("savings")){
+            return new savingsProcessor(bank);
+        }
+        else{
+            return new CDProcessor(bank);
+        }
+    }
+}
